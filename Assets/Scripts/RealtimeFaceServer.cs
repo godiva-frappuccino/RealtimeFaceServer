069037e75154ede729a9023f1fe4e03ca0454b56
godiva-frappuccino <miki.10pro@gmail.com>
2021-07-20 00:15:47 +0900

    implement duration system in behavior

--- Assets/Scripts/RealtimeFaceServer.cs
+++ Assets/Scripts/RealtimeFaceServer.cs
@@ -5,6 +5,7 @@ using System.Net.Sockets;
 using UnityEngine;
 using MiniJSON;
 using System.Linq;
+using System.Collections;
 
 public class RealtimeFaceServer : MonoBehaviour
 {
@@ -12,9 +13,9 @@ public class RealtimeFaceServer : MonoBehaviour
     //目のメッシュ設定
     public MeshRenderer eyeObjL;
     public MeshRenderer eyeObjR;
-
-    private float dirVal = 0;
     private Vector2 eyeOffset;
+
+
     UdpClient udpClient;
     IPEndPoint endPoint;
     private int listenPort = 50007;
@@ -43,14 +44,47 @@ public class RealtimeFaceServer : MonoBehaviour
     {
         //ここでメッセージの処理
         Debug.Log("I Receive " + message);
-        JsonNode jsonNode = JsonNode.Parse(message);
+        string[] parsedMessage = message.Split(':');
         //眼球のxyの値を取得
-        float eyeRotationX = (float)(jsonNode["EyeX"].Get<double>());
-        float eyeRotationY = (float)(jsonNode["EyeY"].Get<double>());
-        UpdatePose(eyeRotationX, eyeRotationY);
+        if (parsedMessage[0].Equals("look"))
+        {
+            float targetX = float.Parse(parsedMessage[1]);
+            float targetY = float.Parse(parsedMessage[2]);
+            Debug.Log("look " + targetX + "," + targetY);
+            int duration = int.Parse(parsedMessage[3]);
+            look(targetX, targetY, duration);
+        }
+
+        //UpdatePose(eyeRotationX, eyeRotationY);
     }
 
+    // message
+    public void look(float targetX, float targetY, int duration)
+    {
+        for(int i = 0; i < duration; i++)
+        {
+            StartCoroutine(DelayCoroutine(0.2f * i, () =>
+            {
+                float speed = 1.0f / duration;
+                float diff_x = targetX - eyeOffset.x;
+                float diff_y = targetY - eyeOffset.y;
+                eyeOffset = new Vector2((eyeOffset.x + (diff_x * speed)) * 0.2f, (eyeOffset.y + (diff_y * speed)) * 0.2f);
+                Debug.Log("Look:" + eyeOffset.x + "," + eyeOffset.y);
+                eyeObjL.material.SetTextureOffset("_MainTex", eyeOffset);
+                eyeObjR.material.SetTextureOffset("_MainTex", eyeOffset);
+            }));
 
+        }
+    }
+
+    // util
+    private IEnumerator DelayCoroutine(float seconds, Action action)
+    {
+        yield return new WaitForSeconds(seconds);
+        action?.Invoke();
+    }
+
+    // old
     public void UpdatePose(float eyeRotationX, float eyeRotationY)
     {
         Debug.Log("Value:" + eyeRotationX + "," + eyeRotationY);
